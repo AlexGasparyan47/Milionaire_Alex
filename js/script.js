003de09d4f11dc9fb6e7af3fe1cf37e78fe5b42e
AlexGasparyan47 <aleks.gasparyan4@tumo.org>
2026-07-24 20:12:29 +0400

    css and js

---
+++ js/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+let  minGame = document.querySelector('.game-block'),
+gameWrapper = document.querySelector('.game-wrap'),
+startBtn = document.querySelector('.start-btn'),
+endbtn = document.querySelector('.end-btn'),
+btnAnswers = document.querySelectorAll('.answer'),
+blockQuestion = document.querySelectorAll('.question'),
+helpBtns = document.querySelectorAll('.hints-help'),
+winBlock = document.querySelectorAll('.wins-block'),
+helpfifty = document.querySelector('.fifty-fifty'),
+helpall = document.querySelector('.hall-help'),
+helpFriend = document.querySelector('.call-friend'),
+helpAi = document.querySelector('.ai-help'),
+game = document.querySelector('.game')
+////////////===========>
+
+let changedQuestion = document.getElementById('hintBox')
+let extraQuestion = document.getElementById('extra')
+let flagExtra = tree
+
+
