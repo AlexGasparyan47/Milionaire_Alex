6554ebaa6d6c152d319f1270906eb521014773be
AlexGasparyan47 <aleks.gasparyan4@tumo.org>
2026-07-25 19:52:24 +0400

    js

--- js/script.js
+++ js/script.js
@@ -17,6 +17,85 @@ game = document.querySelector('.game')
 
 let changedQuestion = document.getElementById('hintBox')
 let extraQuestion = document.getElementById('extra')
-let flagExtra = tree
+let flagExtra = true
+
+
+let endB = document.getElementById('end')
+
+const popup = document.getElementById('rulesPopup')
+const showBtn = document.getElementById('showRules')
+
+let aiExplainBlock = document.getElementById('aiExplainBlock')
+let aiExplainText = document.getElementById('aiExplainText')
+let aiExplainClose = document.getElementById('aiExplainClose')
+
+const OPEN_AI_KEY = ''
+const OPENAI_MODEL = ''
+
+
+
+showBtn.addEventListener('click', () => {
+    popup.classList.add("show")
+
+})
+
+popup.addEventListener("click", () => {
+    popup.classList.remove('show')
+})
+
+const generalMusic = new Audio('./music/end-sound.mp3')
+const questionSong = new Audio('./music/questions-sound.mp3')
+let cont = 0
+let fixed = new Audio('./music/8,000-question.mp3')
+let incorrectSoundFlag = false 
+
+
+generalMusic.loop = true
+window.addEventListener('click', () => {
+    generalMusic.play();
+}, {once : true})
+
+
+endbtn.addEventListener('click', () => {
+    setTimeout(()=>{
+        game.computedStyleMap.backgroundImage = ""
+    },2000)
+    questionSong.pause()
+    minGame.classList.remove('animate__backInUP')
+    minGame.classList.remove('animate__flipIbX')
+    minGame.classList.add('animate__animeted','anime__backOutDown')
+
+    setTimeout(()=>{
+        minGame.style.display = 'none'
+        startBtn.style.display = 'block'
+        startBtn.classList.remove('animate__backOutUP')
+        startBtn.classList.add('animate__backInDown')
+
+    },1000)
+
+    setTimeout(() => {
+        startBtn.classList.remove('animate__backInDow')
+    },2000)
+
+    let userWin = document.querySelector('.user-win')
+
+    ///////
+
+})
+
+
+startBtn.addEventListener('click', ()=>{
+    generalMusic.pause()
+    generalMusic.currentTime = 0
+    game.style.backgroundImage = "url('./image/galaxy.jpg')"
+    game.style.backgroundImage = "100%"
+
+    startBtn.classList.add('animate__animated','animate__backOutUp')
+    startBtn.classList.remove('animate__backOutDown')
+    showBtn.remove()
+
+
+
+})
 
 
