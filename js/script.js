f2136cf2cb5e766fd6307cd65382af825e14ffb7
AlexGasparyan47 <aleks.gasparyan4@tumo.org>
2026-07-29 16:16:19 +0400

    update js and css files

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let  minGame = document.querySelector('.game-block'),
+let  mainGame = document.querySelector('.game-block'),
 gameWrapper = document.querySelector('.game-wrap'),
 startBtn = document.querySelector('.start-btn'),
 endbtn = document.querySelector('.end-btn'),
@@ -46,7 +46,7 @@ popup.addEventListener("click", () => {
 const generalMusic = new Audio('./music/end-sound.mp3')
 const questionSong = new Audio('./music/questions-sound.mp3')
 let cont = 0
-let fixed = new Audio('./music/8,000-question.mp3')
+let fixed1= new Audio('./music/8,000-question.mp3')
 let incorrectSoundFlag = false 
 
 
@@ -79,12 +79,41 @@ endbtn.addEventListener('click', () => {
 
     let userWin = document.querySelector('.user-win')
 
-    ///////
+   if(userWin)
+    userWin.remove()
+
 
 })
+generalMusic.pause()
+fixed1.pause()
+
+let activeWin = document.querySelector('.win-active') || document.querySelector('.win-guaranted')
+if(activeWin){
+    let spans = activeWin.querySelector('span')
+    spans.forEach(spans)
+
+    let visibleAmout = active.innerText.trim()
+    let existingWin = document.querySelector('.user-win')
+    if(existingWin){
+        existingWin.remove()
+    }
+    
+}
+
+let winDiv = document.createElement('div')
+winDiv.className = 'user-win animate_animated animate_fadIn'
+winDiv.style.cssText = 'text-align: center; font-size: 24px; color: white; margin=top: 300px;'
+startBtn.insertAdjacentElement('afterend',winDiv)
+
+
+setTimeout(()=>{
+    winDiv.classList.replace('animate_fedeIn','animate_fadeOut')
+    setTimeout(()=>winDiv.remove())
+},0)
 
 
 startBtn.addEventListener('click', ()=>{
+    
     generalMusic.pause()
     generalMusic.currentTime = 0
     game.style.backgroundImage = "url('./image/galaxy.jpg')"
@@ -96,6 +125,86 @@ startBtn.addEventListener('click', ()=>{
 
 
 
-})
+
+   getStartGame()
+
+   
+
+});
 
 
+// Խաղի սկիզբը
+startBtn.addEventListener('click', () => {//Խաղի սկիզբը կոճակի վրա սեղմելիս , պետք է կատարվեն այս ֆունկցիայում ներառված գործողությւոնները
+    generalMusic.pause();
+    generalMusic.currentTime = 0;
+    game.style.backgroundImage = "url('./img/galaxy.jpg')";
+    game.style.backgroundSize="100%"
+  
+    startBtn.classList.add('animate__animated', 'animate__backOutUp');//նախապես ունեցած կոճակի վրա ավելացնում ենք այս երկու անիմացիաները
+    mainGame.classList.remove('animate__backOutDown');//mainGame-ից հեռացնում ենք այս կլաս անուն ունեցող անիմացիան
+    showBtn.remove()
+    setTimeout(() => {//Ցույց է տալիս թե ինչքան ժամանակ հետո պետք է կատարվի տվյալ գործողությունը
+      mainGame.style.display = 'block';
+      mainGame.classList.add('animate__animated', 'animate__backInUp');//mainGame-ին ավելացնումէ է նախապես ստեղծված  կլաս անվանում
+      startBtn.style.display = 'none';
+      setTimeout(() => {
+        gameWrapper.classList.add('animate__animated', 'animate__flipInX');//gameWrapper-ին ավելացնումէ է նախապես ստեղծված  կլաս անվանում
+      }, 1000);
+    }, 500);
+    setTimeout(() => {
+      endBtn.style.opacity = '1';// տրված է առավելագույն թափանցելիություն
+    }, 1000);
+    //
+    setTimeout(() => {
+      questionSong.loop = true
+      questionSong.play()
+      for (let i = 0; i < btnAnswers.length; i++) {
+        btnAnswers[i].addEventListener('click', () => {
+          questionSong.pause()
+          setTimeout(() => {
+            if (count != 5 && count != 10 && count != 15) {
+              if (incorrectSoundFlag == false && count < 5) {
+                questionSong.play()
+              }
+              if (count == 15) {
+                fixed1.pause()
+              }
+              questionSong.currentTime = 0
+            } else if (count >= 5) {
+              fixed1.loop = true
+              fixed1.play()
+              questionSong.pause()
+            }
+          }, 3000);
+  
+        })
+      }
+    }, 2000);
+  });
+
+  btnAnswers.forEach((btnAnswers)=>{
+    btnAnswers.addEventListener('click', (e)=>{
+        let numberQuestion = btnAnswers.parentElement.parentElement.classList[1]
+
+        let userAnswer = e.target.innerText
+        let blockAnswer = e.target
+        let blockQuestionParentElement = blockAnswer.parentElement
+
+        blockQuestionParentElement.class.add('block-event')
+        correctnessAnswer(numberQuestion,userAnswer,blockAnswer,blockQuestionParentElement)
+       
+    })
+
+  })
+
+  btnAnswers.forEach(()=>{
+    item.addEventListener('mouseover', ()=>{
+        if(item.children[0]){
+            item.children(0).style.display = "none"
+            item.classList.remove('color-active')
+
+        }
+    });
+  });
+
+  let helpSound = new Audio('./music/50-50 .mp3')
